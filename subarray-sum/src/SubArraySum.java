c21c40441b14257d6923f2ce41722df74acb7e92
Nitay Caspi <NitayCaspi12@gmail.com>
2021-11-10 11:55:55 +0200

    init

---
+++ subarray-sum/src/SubArraySum.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class SubArraySum {
+    static final Scanner SCANNER = new Scanner(System.in);
+    public static void main(String[] args) {
+     int[] arr = generateArr();
+     int count = subarraySum(arr);
+        System.out.println("The number of sub arrays equals to 0 is: " + count);
+    }
+    static int subarraySum(int[] arr) {
+        int count = 0;
+        for (int i=0; i< arr.length; i++) {
+            int sum=0;
+            for (int j= i; j < arr.length; j++) {
+                sum+=arr[j];
+                if (sum == 0)
+                    count++;
+            }
+        }
+        return count;
+    }
+
+    // Generating an array from user input
+    static int[] generateArr(){
+        System.out.print("How many numbers would you like to enter?: ");
+        int arrLength = SCANNER.nextInt();
+        int[] arr = new int[arrLength];
+        for (int i = 0; i < arrLength; i++){
+            System.out.print("#"+(i+1)+" Please enter a whole number: ");
+            arr[i]=SCANNER.nextInt();
+        }
+        return arr;
+    }
+}
